ddef8ba5927ee26fa661dfe918aa031087de70d8
Wasim Gourh <38152653+wes-gourh@users.noreply.github.com>
2020-11-02 16:13:59 +0530

    added route to meal_detail_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'screens/category_meals_screen.dart';
-import 'screens/categories_screen.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
       home: CategoriesScreen(),
       routes: {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
     );
   }
